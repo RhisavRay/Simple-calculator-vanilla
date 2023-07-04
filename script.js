4350fd62ca62786100ab0f48390e8c6ba62128eb
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-04 23:46:11 +0530

    script done

--- script.js
+++ script.js
@@ -59,3 +59,32 @@ function handleMath(symbol)
     previousOperator = symbol
     buffer = '0'
 }
+
+function flushOperation(intBuffer)
+{
+    if(previousOperator === '+')
+        runningTotal += intBuffer
+    else if(previousOperator === '-')
+        runningTotal -= intBuffer
+    else if(previousOperator === '×')
+        runningTotal *= intBuffer
+    else if(previousOperator === '÷')
+        runningTotal /= intBuffer
+}
+
+function handleNumber(numberString)
+{
+    if(buffer === '0')
+        buffer = numberString
+    else
+        buffer += numberString
+}
+
+function init()
+{
+    document.querySelector('.calc_buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText)
+    })
+}
+
+init()
